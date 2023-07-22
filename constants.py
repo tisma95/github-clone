24a5a5e9686a64ecb6434cad4d04d70b92f341a4
Ismaël Maurice <ismael.tuo@edigrp.com>
2023-07-22 09:11:14 +0200

    chore(constants): init the constant variables file

---
+++ constants.py
@@ -0,0 +1,12 @@
+#################################################################
+#               Author: Ismael Maurice                          #
+#               Language: Python                                #
+#               Projet: Github Clone                            #
+#               Version: V1                                     #
+#               File: constants.py                              #
+#################################################################
+
+# APPLICATION NAME
+APP_NAME = "Github Clone"
+# API URL TYPE
+API_URL_TYPE = "API"
